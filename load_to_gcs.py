ce74578af7e3352bec326710253adb361178594b
Ondrej <49439520+OndrejVejvoda@users.noreply.github.com>
2023-11-22 09:54:34 +0100

    Update load_to_gcs.py
    Update code for env variables

--- load_to_gcs.py
+++ load_to_gcs.py
@@ -3,11 +3,11 @@ import pandas as pd
 from google.cloud import storage
 from scrape_to_csv import *
 
-def upload_to_gcs():
+def upload_to_gcs(bucket_name, blob_name):
     # Configuration (better to move this to environment variables or a config file)
-    bucket_name = 'fight_stats_data'
-    blob_name = 'raw/raw_fighters.csv'
-    service_account_key_path = 'fightstats-404410-cf30b6b920d1.json'
+    #bucket_name = 'fight_stats_data'
+    #blob_name = 'raw/raw_fighters.csv'
+    #service_account_key_path = 'fightstats-404410-cf30b6b920d1.json'
 
      # Get data and convert to CSV format
     fighters_data = scrape_data()  # Assuming this function returns the required data
@@ -16,7 +16,7 @@ def upload_to_gcs():
 
     try:
         # Authenticate and initialize the client
-        os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = service_account_key_path
+        #os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = service_account_key_path
         storage_client = storage.Client()
 
         # Get the bucket object and upload the data
@@ -29,4 +29,6 @@ def upload_to_gcs():
         print(f"Failed to upload the file: {str(e)}")
 
 if __name__ == "__main__":
-    upload_to_gcs()
+    bucket_name = os.getenv('BUCKET_NAME')
+    blob_name = os.getenv('BLOB_NAME')
+    upload_to_gcs(bucket_name, blob_name)
